d2ec2deea2b5cf2b7868290a4b7b08bab93d85c1
Norm <njgallardo@gmail.com>
2020-09-03 05:10:15 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-alert('Hello world');
-
-let favoriteFood = 'peanut_butter';
-document.write(favoriteFood);
+let pokemonList = [
+  {name: 'Bulbasaur', height: 7, types:['grass', 'poison']},
+  {name: 'Charmander', height: 6, types:['fire']},
+  {name: 'Sandshrew', height: 6, types:['ground']},
+  {name: 'Pikachu', height: 4, types:['electric]},
+  ];
