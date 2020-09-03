0344d262c7cec5d18b08b34a3ed801cd7ea2a35b
Norm <njgallardo@gmail.com>
2020-09-03 05:35:04 -0700

    Update scripts.js
    Task 2 of JS. Created pokemonList array with 4 objects.

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,5 @@ let pokemonList = [
   {name: 'Bulbasaur', height: 7, types:['grass', 'poison']},
   {name: 'Charmander', height: 6, types:['fire']},
   {name: 'Sandshrew', height: 6, types:['ground']},
-  {name: 'Pikachu', height: 4, types:['electric]},
+  {name: 'Pikachu', height: 4, types:['electric']},
   ];
