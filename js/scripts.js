f7d32c95650c35650d956e6cf9500037f51d1b66
Norm <njgallardo@gmail.com>
2020-09-13 04:23:57 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,13 @@ let pokemonList = [
   {name: 'Pikachu', height: 4, types:['electric']},
   ];
 
+//The below code creates a for loop that will iterate over each 
 for (let i=0; i < pokemonList.length; i++) {
-  document.write(pokemonList[i].name + '(height:' + pokemonList[i].height + ')');
+  //added a conditional 'if' statement to find items that have height greater thatn 6
+  if (pokemonList[i].height > 6){
+      document.write(pokemonList[i].name + '(height:' + pokemonList[i].height + ')' + ' - Wow, that’s big!');
+  //the else statement returns the rest of the items without the special message  
+  }else {
+      document.write(pokemonList[i].name + '(height:' + pokemonList[i].height + ')');
+  }
 }
